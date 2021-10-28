8d6aa47dd09e0fcb6c655305f347bcc9abd47679
James Jerkowski <jerkowski@gmail.com>
2021-10-27 21:16:10 -0400

    Initial Commit

---
+++ src/components/Details/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: '10px solid rgba(1, 179, 118, 0.7)',
+  },
+  expense: {
+    borderBottom: '10px solid rgba(229, 9, 20, 0.7)',
+  },
+}));
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
+import currencyFormat from '../../utils/currencyFormat';
+
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
+import InfoCard from '../InfoCard';
+
+const Main = () => {
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
+
+  return (
+    <Card className={classes.root} >
+      <CardHeader title="ExpenseTracker" subheader="By James Jerkowski" />
+      <CardContent>
+        <Typography align="center" variant="h5">Total Balance {currencyFormat(balance)}</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+          <InfoCard />
+        </Typography>
+        <Divider className={classes.divider} />
+        <Form />
+        <CardContent className={classes.cardContent}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </CardContent>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Main;
